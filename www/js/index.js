e515b7d20acaefb2a58679d99ea6f4a1abb8d078
Dan Phifer <dphifer@fixmusicapp.com>
2019-09-25 13:23:51 -0400

    Change this branch to just use one approach for clarity

--- www/js/index.js
+++ www/js/index.js
@@ -33,11 +33,7 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function (id) {
         let parentElement = document.getElementById(id);
-        let cameraElement = parentElement.querySelector('.camera');
-
-        cameraElement.onclick = () => captureVideoErrorOnAndroid();
-
-        parentElement.querySelector('.camera2').onclick = () => videoCapture();
+        parentElement.querySelector('.camera').onclick = () => videoCapture();
         log(id);
     }
 };
@@ -50,31 +46,8 @@ function log(text) {
     document.querySelector("ul.logs").appendChild(node);
 }
 
-function captureVideoErrorOnAndroid() {
-    const printError = (e) => console.log(e);
-    navigator.device.capture.captureVideo(function (mediaFiles) {
-        const mediaFile = mediaFiles[0];
-        log(`Got media file: ${JSON.stringify(mediaFile)}`)
-
-        // note, on iOS I have to use media.localURL rather than media.fullPath
-        // I also tried media.localURL on Android, but the results are the same 
-        window.resolveLocalFileSystemURL(mediaFile.fullPath, function (fileEntry) {
-            log(`Got fileEntry: ${JSON.stringify(fileEntry)}`)
-            fileEntry.file(function (file) {
-                log(`Got file: ${JSON.stringify(file)} `)
-                const reader = new FileReader();
-
-                reader.onloadend = function () {
-                    log(`Result: ${this.result}, error: ${JSON.stringify(this.error)}`)
-                };
-                reader.readAsArrayBuffer(file);
-
-            }, printError);
-        }, printError)
-    }, printError, { limit: 1, quality: 0, duration: 10 })
-}
-
 /**
+ * Method 2
  * Adding sample code from this issue https://github.com/apache/cordova-plugin-media-capture/issues/135
  * to make sure we're doing the same thing
  */
@@ -94,15 +67,14 @@ function videoCapture() {
         );
     })
       .then(mediaFile => successVideo(mediaFile))
-      .then(result => log(JSON.stringify(result)))
       .catch(err => {
-          log(err);
           console.log(err);
       });
 }
 
 function successVideo(mediaFile) {
     let videoUrl = mediaFile.fullPath;
+    log('Full path: ' + videoUrl);
 
     return new Promise((resolve, reject) => {
         window.resolveLocalFileSystemURL(
@@ -113,10 +85,13 @@ function successVideo(mediaFile) {
                     setTimeout(() => {
                         const reader = new FileReader();
                         reader.onloadend = function () {
-                            resolve([this.result, file, entry]);
+                          log("reader.onloadend");
+                          log(`capture: result: ${this.result}, error: ${JSON.stringify(this.error)}`);
+                          resolve([this.result, file, entry]);
                         };
                         reader.onerror = function (ev) {
-                            reject(ev);
+                          log("reader.onerror: " + JSON.stringify(ev));
+                          reject(ev);
                         };
                         reader.readAsArrayBuffer(file);
                     }, 1000);
